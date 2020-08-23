12f6e9243aaa74e9e5ccb49712a0c31df3fd9681
Georg Pelz <hybrid@selur.de>
2020-08-23 11:10:02 +0200

    init

---
+++ deploy-tools.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+readme_file="README"
+binaries_copy="nvhsp"
+binaries_qt="Hybrid bdsup2sub++ d2vwitch delaycut FrameCounter IdxSubCutter vsViewer"
+binaries_32bit="DivX265 neroAacEnc tsMuxeR"
+binaries_64bit="""
+aac-enc
+aften
+aomenc
+faac
+ffdcaenc
+ffmbc
+ffmpeg
+ffmsindex
+flac
+FLVExtractCL
+kvazaar
+lame
+lsdvd
+mediainfo
+mencoder
+mkvextract
+mkvinfo
+mkvmerge
+MP4Box
+mp4fpsmod
+mplayer
+oggenc
+opusenc
+rav1e
+sox
+telxcc
+vpxenc
+x264
+x265
+SvtHevcEncApp
+SvtAv1EncApp
+"""
+
+deploy_dir="hybrid"
+
+set -e
+set -x
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install --no-install-recommends -y \
+  build-essential \
+  autoconf \
+  automake \
+  fuse \
+  git \
+  wget \
+  p7zip-full \
+  libqt5multimedia5 \
+  libqt5multimedia5-plugins \
+  libqt5xml5 \
+  libfreetype6:i386 \
+  zlib1g:i386 \
+  libgcc1:i386 \
+  libstdc++6:i386
+
+cd tools
+rm -rf $deploy_dir
+mkdir $deploy_dir
+cd $deploy_dir
+
+git clone --depth=1 https://github.com/Selur/VapoursynthScriptsInHybrid vsscripts
+rm -rf vsscripts/.git
+
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+chmod a+x *.AppImage
+
+# https://github.com/linuxdeploy/linuxdeploy/issues/86
+#sed -i 's|AI\x02|\x00\x00\x00|' *.AppImage
+
+cmdLine="--appdir=. --plugin qt -l/usr/lib/i386-linux-gnu/libfreetype.so.6 -l/lib/i386-linux-gnu/libz.so.1"
+for bin in $binaries_qt $binaries_64bit $binaries_32bit ; do
+  cmdLine="$cmdLine -e../$bin"
+done
+echo "call: ./linuxdeploy-x86_64.AppImage $cmdLine"
+./linuxdeploy-x86_64.AppImage $cmdLine
+cp ../*.txt ./usr/share/doc
+
+cd ..
+cp $binaries_copy ./$deploy_dir/usr/bin
+cp $readme_file ./$deploy_dir
+cd $deploy_dir
+
+mv ./usr/* .
+mv ./bin/* .
+mv ./share/doc .
+
+git clone https://github.com/NixOS/patchelf
+cd patchelf
+./bootstrap.sh
+./configure
+make
+cd ..
+
+for bin in $binaries_qt $binaries_64bit ; do
+  ./patchelf/src/patchelf --set-rpath '$ORIGIN/lib' $bin
+done
+
+for bin in $binaries_32bit ; do
+  ./patchelf/src/patchelf --set-rpath '$ORIGIN/lib32' $bin
+done
+
+chmod a+x $binaries_qt $binaries_64bit $binaries_32bit $binaries_copy
+
+cat <<EOF >qt.conf
+[Paths]
+Prefix = .
+Plugins = plugins
+Imports = qml
+Qml2Imports = qml
+EOF
+
+rm -rf ./usr ./bin ./share ./patchelf *.AppImage
+cd ..
+
+#rsync -r build/* $deploy_dir/vsfilters/
+now=$(date +"%Y%m%d")
+7z a -m0=lzma2 -mx "../Hybrid_$now.7z" $deploy_dir
+
